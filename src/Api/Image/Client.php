2ea22a048fc0d5a4572bbd630f26d5669cef70ba
xiaoxuan6 <1527736751@qq.com>
2023-07-28 19:38:54 +0800

    fix: Update image api

--- src/Api/Image/Client.php
+++ src/Api/Image/Client.php
@@ -17,6 +17,11 @@ use Vinhson\Search\Api\Kernel\{BaseClient, WaterMark};
 
 class Client extends BaseClient
 {
+    public array $headers = [
+        'User-Agent' => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+        'Content-Type' => 'application/json'
+    ];
+
     /**
      *  图片添加水印
      *
@@ -53,10 +58,7 @@ class Client extends BaseClient
                     'textSize' => $fontsize,
                     'transparent' => 100
                 ],
-                'headers' => [
-                    'User-Agent' => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-                    'Content-Type' => 'application/json'
-                ]
+                'headers' => $this->headers
             ]
         );
 
@@ -65,5 +67,61 @@ class Client extends BaseClient
         }
 
         throw new InvalidArgumentException(sprintf("添加水印失败：%s", $response->getMessage('msg')));
+    }
+
+    /**
+     * 图片转像素画
+     *
+     * @param string $image 图片
+     * @param int $pixelRatio 像素
+     * @return string
+     */
+    public function pixel(string $image, int $pixelRatio = 10): string
+    {
+        $response = $this->client->post(
+            sprintf("%s/base/gtool/api/v1/PixelImage", trim($this->config->get('image.url'), '/')),
+            [
+                'json' => [
+                    'format' => pathinfo($image, PATHINFO_EXTENSION),
+                    'fileBase64Content' => base64_encode(file_get_contents($image)),
+                    'transparent' => (0 < $pixelRatio and $pixelRatio > 20) ? $pixelRatio : 10
+                ],
+                'headers' => $this->headers
+            ]
+        );
+
+        if ($response->isSuccess() and $response->getData('code') == 0) {
+            return $response->getData('data');
+        }
+
+        throw new InvalidArgumentException(sprintf("图片转像素画失败：%s", $response->getMessage('msg')));
+    }
+
+    /**
+     * 图片添加毛玻璃
+     *
+     * @param string $image
+     * @param int $glassRatio
+     * @return string
+     */
+    public function frostedGlass(string $image, int $glassRatio = 5): string
+    {
+        $response = $this->client->post(
+            sprintf("%s/base/gtool/api/v1/AeroGlassImage", trim($this->config->get('image.url'), '/')),
+            [
+                'json' => [
+                    'format' => pathinfo($image, PATHINFO_EXTENSION),
+                    'fileBase64Content' => base64_encode(file_get_contents($image)),
+                    'glassRatio' => (0 < $glassRatio and $glassRatio > 10) ? $glassRatio : 5
+                ],
+                'headers' => $this->headers
+            ]
+        );
+
+        if ($response->isSuccess() and $response->getData('code') == 0) {
+            return $response->getData('data');
+        }
+
+        throw new InvalidArgumentException(sprintf("图片添加毛玻璃失败：%s", $response->getMessage('msg')));
     }
 }
