41786b8ec7f19be2cf247a64d6d9c89506c697d1
xiaoxuan6 <1527736751@qq.com>
2024-04-10 14:08:03 +0800

    refactor: push command

--- src/Commands/PushCommand.php
+++ src/Commands/PushCommand.php
@@ -12,10 +12,12 @@
 
 namespace Vinhson\Search\Commands;
 
+use Illuminate\Support\Collection;
 use Symfony\Component\Process\Process;
-use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\{InputArgument, InputInterface, InputOption};
+
+use function Laravel\Prompts\text;
 
 class PushCommand extends Command
 {
@@ -24,7 +26,7 @@ class PushCommand extends Command
         $this->setName('git:push')
             ->setAliases(['gh'])
             ->setDescription('git 提交数据')
-            ->addArgument('message', InputArgument::OPTIONAL, 'git 提交信息')
+            ->addArgument('message', InputArgument::OPTIONAL, 'git 提交信息', '')
             ->addOption('amend', 'a', InputOption::VALUE_OPTIONAL, '是否修改最后一次提交信息', false)
             ->addOption('no-edit', 'e', InputOption::VALUE_OPTIONAL, '是否使用最后一次提交信息', false)
             ->addOption('force', 'f', InputOption::VALUE_OPTIONAL, '是否强制提交', false)
@@ -38,61 +40,56 @@ class PushCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $message = 'fix: wip';
-        if(! $noEdit = $input->getOption('no-edit')) {
-            MSG:
-            if (! $message = $input->getArgument('message')) {
-                $helper = $this->getHelper('question');
-                $question = new Question("<info>请输入提交信息：</info>", '');
-                if (! $answer = $helper->ask($input, $output, $question)) {
-                    goto MSG;
-                }
-
-                $message = $answer;
-            }
-        }
-
-        $commands = collect([
+        return collect([
             'git pull',
             'git status',
             'git add .'
-        ]);
+        ])->tap(function () use ($input, &$target, &$noEdit, &$message): void {
+            $target = true;
+            $noEdit = $input->getOption('no-edit');
 
-        if ($input->getOption('amend') && $noEdit) {
-            $commands->push(...[
+            $message = str($input->getArgument('message'))
+                ->trim()
+                ->whenEmpty(fn (): string => text(
+                    label: '请输入提交信息',
+                    validate: fn ($value): string => str($value)->trim()->isEmpty() ? '提交信息不能为空' : true
+                ))
+                ->toString();
+        })->when($input->getOption('amend') && $noEdit, function (Collection $collect, $value) use (&$target): Collection {
+            $target = false;
+
+            return $collect->push(...[
                 'git commit --amend --no-edit',
                 'git push -f'
             ]);
-        } elseif($input->getOption('amend')) {
-            $commands->push(...[
+        })->when($input->getOption('amend') && ! $noEdit, function (Collection $collect, $amend) use ($message, &$target): Collection {
+            $target = false;
+
+            return $collect->push(...[
                 'git commit --amend -m"' . $message . '"',
                 'git push -f'
             ]);
-        } elseif ($input->getOption('force')) {
-            $commands->push(...[
+        })->when($input->getOption('force'), function (Collection $collect, $force) use ($message, &$target): Collection {
+            $target = false;
+
+            return $collect->push(...[
                 'git commit -m"' . $message . '"',
                 'git push -f'
             ]);
-        } else {
-            $commands->push(...[
-                'git commit -m"' . $message . '"',
-                'git push'
-            ]);
-        }
+        })->when($target, fn (Collection $collect, $value): Collection => $collect->push(...[
+            'git commit -m"' . $message . '"',
+            'git push'
+        ]))->when($input->getOption('tag'), fn (Collection $collect, $tag): Collection => $collect->push(...["git tag {$tag}", "git push origin {$tag}"]))->pipe(function (Collection $collect) use ($output): int {
+            $process = Process::fromShellCommandline($collect->join(' && '), getcwd());
+            $process->run(fn ($type, $line) => $output->writeln($line));
 
-        if ($tag = $input->getOption('tag')) {
-            $commands->push(...["git tag {$tag}", "git push origin {$tag}"]);
-        }
+            if (! $process->isSuccessful()) {
+                $output->writeln("<error>提交失败：{$process->getErrorOutput()}</error>");
 
-        $process = Process::fromShellCommandline($commands->join(' && '), getcwd());
-        $process->run(function ($type, $line) use ($output): void {
-            $output->writeln($line);
+                return self::FAILURE;
+            }
+
+            return self::SUCCESS;
         });
-
-        if (! $process->isSuccessful()) {
-            $output->writeln("<error>提交失败：{$process->getErrorOutput()}</error>");
-        }
-
-        return self::FAILURE;
     }
 }
